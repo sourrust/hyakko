335698bc76a5479b6385627255028767b3c9e4fa
Jeremy Hull <sourdrums@gmail.com>
2011-09-25 20:24:55 -0400

    Implement interface for translating Markdown.

--- src/Text/Markdown.hs
+++ src/Text/Markdown.hs
@@ -1,2 +1,13 @@
+module Text.Markdown (toHTML) where
+
+import Text.Pandoc
+  ( readMarkdown
+  , writeHtmlString
+  , defaultParserState
+  , defaultWriterOptions
+  )
 
 toHTML :: String -> String
+toHTML s =
+  let parse = readMarkdown defaultParserState s
+  in writeHtmlString defaultWriterOptions parse
