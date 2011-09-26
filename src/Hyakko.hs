49c341686fb8fbe0bcca69ef59351dfa85a683dd
Jeremy Hull <sourdrums@gmail.com>
2011-09-25 20:51:16 -0400

    Implement generateHTML and languages
    * generateHTML is really complex because Pandoc's template engine isn't
      that powerful.
    * For now Haskell is the only thing in languages

--- src/Hyakko.hs
+++ src/Hyakko.hs
@@ -6,7 +6,7 @@ import qualified Data.Map as M
 import Data.List (sort)
 import Text.Pandoc.Templates
 import System.Environment (getArgs)
-import System.FilePath (takeBaseName)
+import System.FilePath (takeBaseName, takeExtension, takeFileName)
 import System.Process (system)
 
 type Callback = IO ()
@@ -24,10 +24,38 @@ parse :: FilePath -> String -> [M.Map String String]
 highlight :: FilePath -> [M.Map String String] -> Callback -> IO ()
 
 generateHTML :: FilePath -> [M.Map String String] -> IO ()
+generateHTML src sections = do
+  let title = takeFileName src
+      dest  = destination src
+  source <- sources
+  html <- hyakkoTemplate $ concat [
+    [("title", title)],
+    if length source > 1 then [("multi","1")] else [],
+    map (\x -> ("source", unlines [
+      "<a class='source' href='"++(takeFileName $ destination x)++"'>",
+      "  "++takeFileName x,"</a>"])) source,
+    map (\x -> ("section", unlines [
+      "<tr id='section-"++show (x + 1)++"'>",
+      "  <td class='docs'>",
+      "    <div class='pilwrap'>",
+      "      <a class='pilcrow' href='#section-"++show (x + 1)++"'>&#955;</a>",
+      "    </div>",
+      (section !! x) M.! "docsHtml",
+      "  </td>",
+      "  <td class='code'>",
+      (section !! x) M.! "codeHtml",
+      "  </td>",
+      "</tr>" ])) [0..(length section) - 1]
+    ]
 
 languages :: M.Map String (M.Map String String)
+languages = M.fromList [
+            (".hs", M.fromList [
+              ("name", "haskell"), ("symbol", "--")])
+            ]
 
 getLanguage :: FilePath -> M.Map String String
+getLanguage src = languages M.! takeExtension src
 
 destination :: FilePath -> FilePath
 destination fp = "docs/" ++ (takeBaseName fp) ++ ".html"
@@ -52,7 +80,8 @@ sources = getArgs >>= return . sort
 main :: IO ()
 main = do
   style <- hyakkoStyles
+  source <- sources
   putStrLn style
   ensureDirectory $ do
     writeFile "docs/hyakko.css" style
-    generateDocumentation sources
+    generateDocumentation source
