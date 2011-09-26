aabe6e318c8fdd649afd557643a944a761266105
Jeremy Hull <sourdrums@gmail.com>
2011-09-25 20:26:25 -0400

    Implement the simple functions

--- src/Hyakko.hs
+++ src/Hyakko.hs
@@ -1,6 +1,22 @@
+module Main where
+
+import Text.Markdown
+
+import Data.List (sort)
+import Text.Pandoc.Templates
+import System.Environment (getArgs)
+import System.FilePath (takeBaseName)
+import System.Process (system)
+
 type Callback = IO ()
 
-generateDocumentation :: FilePath -> IO ()
+generateDocumentation :: [FilePath] -> IO ()
+generateDocumentation (x:xs) = do
+  code <- readFile x
+  let sections = parse x code
+  highlight x sections $ do
+    generateHTML x sections
+    if null xs then return () else generateDocumentation xs
 
 parse :: FilePath -> String -> [(String, String)]
 
@@ -13,15 +29,29 @@ languages :: [(String, [(String, String)])]
 getLanguage :: FilePath -> [(String, String)]
 
 destination :: FilePath -> FilePath
+destination fp = "docs/" ++ (takeBaseName fp) ++ ".html"
 
 ensureDirectory :: Callback -> IO ()
+ensureDirectory cb = system "mkdir -p docs" >> cb
 
 hyakkoTemplate :: [(String, String)] -> IO String
+hyakkoTemplate var = readFile "resources/hyakko.html" >>=
+  return . renderTemplate var
 
 hyakkoStyles :: IO String
+hyakkoStyles = readFile "resources/hyakko.css"
 
 highlightStart, highlightEnd :: String
+highlightStart = "<div class=\"highlight\"><pre>"
+highlightEnd = "</pre></div>"
 
-sources :: [FilePath]
+sources :: IO [FilePath]
+sources = getArgs >>= return . sort
 
 main :: IO ()
+main = do
+  style <- hyakkoStyles
+  putStrLn style
+  ensureDirectory $ do
+    writeFile "docs/hyakko.css" style
+    generateDocumentation sources
