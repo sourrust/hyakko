3bc751c32063e8f4c846f2ea7fc91af4671f15ac
Christoph Pohl <schnasni@lavabit.com>
2013-02-03 21:57:00 +0100

    Changed Markdown.hs for Pandoc >=1.10 compatibility

--- src/Text/Markdown.hs
+++ src/Text/Markdown.hs
@@ -3,14 +3,18 @@ module Text.Markdown (toHTML) where
 import Text.Pandoc
   ( readMarkdown
   , writeHtmlString
-  , defaultParserState
-  , defaultWriterOptions
+  , ReaderOptions
+  , WriterOptions
+  , def
   )
+
 import Data.ByteString.Lazy.Char8 (ByteString, pack)
 
+readMD = readMarkdown def
+writeHTMLStr = writeHtmlString def
 -- Function for translating Markdown to HTML since `Pandoc` has several
 -- different generators for other markup languages.
 toHTML :: String -> ByteString
-toHTML = pack . writeHtmlString defaultWriterOptions . parse
-  where parse = readMarkdown defaultParserState
+toHTML = pack . writeHTMLStr . parse
+  where parse = readMD 
 {-# INLINE toHTML #-}
